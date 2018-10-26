a17b4803421c3436d841df086eee04fb7dfd19e1
Nathan Fernandes <nathanfpedroza@gmail.com>
2018-10-26 18:14:40 -0300

    styles and modules
    asdasdasd

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  //state = {}
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
   render() {
     React.createElement("div");
     return (
-      <div>
-        <h1>Hello caralho</h1>
-        <button>Increment</button>
-      </div>
+      <React.Fragment>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
