6a9a9e8f4fde63560695ea06189f5946b9c2ca1a
Nathan Fernandes <nathanfpedroza@gmail.com>
2018-10-26 18:19:51 -0300

    asdasdbh
    fvfdvf

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -5,12 +5,18 @@ class Counter extends Component {
     count: 0,
     imageUrl: "https://picsum.photos/200"
   };
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
 
   render() {
     React.createElement("div");
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
