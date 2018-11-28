6947a0340e4741648d996e283ffe014dd4201bca
Nathan Fernandes <nathanfpedroza@gmail.com>
2018-11-28 00:08:58 -0300

    ..

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -15,3 +15,4 @@ class Counters extends Component {
     );
   }
 }
+export default Counters;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counterComponent.jsx";
-import Background from "./components/backgroundComp.jsx";
+//import Background from "./components/backgroundComp.jsx";
 import Counters from "./components/counters.jsx";
 
 ReactDOM.render(<Counters />, document.getElementById("root"));
